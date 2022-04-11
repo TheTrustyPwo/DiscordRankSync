0ab74a0d44c7f8c8f04e15bf44d709d1acd7fe44
TheTrustyPwo <shucecai@gmail.com>
2022-04-11 17:13:09 +0800

    Make discord message embed configurable

--- src/main/java/net/evilkingdom/discordranks/events/discord/LinkSlashCommand.java
+++ src/main/java/net/evilkingdom/discordranks/events/discord/LinkSlashCommand.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.evilkingdom.discordranks.DiscordRankSync;
 import org.bukkit.Bukkit;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class LinkSlashCommand extends ListenerAdapter {
@@ -22,7 +23,14 @@ public class LinkSlashCommand extends ListenerAdapter {
         String code = event.getOption("code").getAsString();
 
         if (!this.plugin.getPlayerManager().getSecretCodes().containsKey(code)) {
-            event.reply("invalid code").setEphemeral(true).queue();
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setTitle(this.plugin.getConfig().getString("messages.discord.link_invalid.title"));
+            embedBuilder.setDescription(this.plugin.getConfig().getString("messages.discord.link_invalid.description"));
+            embedBuilder.setColor(Integer.parseInt(this.plugin.getConfig().getString("messages.discord.link_invalid.color"), 16));
+            if (this.plugin.getConfig().getBoolean("messages.discord.link_invalid.timestamp")) {
+                embedBuilder.setTimestamp(new Date().toInstant());
+            }
+            event.replyEmbeds(embedBuilder.build()).queue();
             return;
         }
 
@@ -33,9 +41,14 @@ public class LinkSlashCommand extends ListenerAdapter {
         this.plugin.getPlayerManager().link(uuid, event.getUser().getId());
 
         EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle("Success!");
-        embedBuilder.setDescription(String.format("You are now linked with %s", playerName));
+        embedBuilder.setTitle(this.plugin.getConfig().getString("messages.discord.link_success.title"));
+        embedBuilder.setDescription(this.plugin.getConfig().getString("messages.discord.link_success.description")
+                .replace("%player-name%", playerName));
+        embedBuilder.setColor(Integer.parseInt(this.plugin.getConfig().getString("messages.discord.link_success.color"), 16));
+        if (this.plugin.getConfig().getBoolean("messages.discord.link_success.timestamp")) {
+            embedBuilder.setTimestamp(new Date().toInstant());
+        }
 
-        event.replyEmbeds(embedBuilder.build()).setEphemeral(true).queue();
+        event.replyEmbeds(embedBuilder.build()).queue();
     }
 }
