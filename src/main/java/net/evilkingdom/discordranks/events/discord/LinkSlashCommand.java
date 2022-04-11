a6644d7c1e852be0c1ad10b12dbed01cf98585ad
TheTrustyPwo <shucecai@gmail.com>
2022-04-11 18:06:57 +0800

    Load embeds on startup

--- src/main/java/net/evilkingdom/discordranks/events/discord/LinkSlashCommand.java
+++ src/main/java/net/evilkingdom/discordranks/events/discord/LinkSlashCommand.java
@@ -6,9 +6,9 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.evilkingdom.discordranks.DiscordRankSync;
 import org.bukkit.Bukkit;
 
-import java.util.Date;
 import java.util.UUID;
 
+@SuppressWarnings("ConstantConditions")
 public class LinkSlashCommand extends ListenerAdapter {
     private final DiscordRankSync plugin;
 
@@ -23,31 +23,29 @@ public class LinkSlashCommand extends ListenerAdapter {
         String code = event.getOption("code").getAsString();
 
         if (!this.plugin.getPlayerManager().getSecretCodes().containsKey(code)) {
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setTitle(this.plugin.getConfig().getString("messages.discord.link_invalid.title"));
-            embedBuilder.setDescription(this.plugin.getConfig().getString("messages.discord.link_invalid.description"));
-            embedBuilder.setColor(Integer.parseInt(this.plugin.getConfig().getString("messages.discord.link_invalid.color"), 16));
-            if (this.plugin.getConfig().getBoolean("messages.discord.link_invalid.timestamp")) {
-                embedBuilder.setTimestamp(new Date().toInstant());
-            }
-            event.replyEmbeds(embedBuilder.build()).queue();
+            event.replyEmbeds(this.plugin.getEmbed("invalid_code")).queue();
             return;
         }
 
         UUID uuid = this.plugin.getPlayerManager().getSecretCodes().get(code);
         String playerName = Bukkit.getOfflinePlayer(uuid).getName();
 
+        if (this.plugin.getPlayerManager().getPlayerUserCache().containsValue(event.getUser().getId())) {
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.copyFrom(this.plugin.getEmbed("already_linked"));
+            embedBuilder.setDescription(embedBuilder.getDescriptionBuilder().toString().replace("%player-name%", playerName));
+            event.replyEmbeds(embedBuilder.build()).queue();
+            return;
+        }
+
         this.plugin.getPlayerManager().getSecretCodes().remove(code);
         this.plugin.getPlayerManager().link(uuid, event.getUser().getId());
 
         EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle(this.plugin.getConfig().getString("messages.discord.link_success.title"));
-        embedBuilder.setDescription(this.plugin.getConfig().getString("messages.discord.link_success.description")
+        embedBuilder.copyFrom(this.plugin.getEmbed("link_success"));
+        embedBuilder.setDescription(embedBuilder.getDescriptionBuilder().toString().replace("%player-name%", playerName));
+        embedBuilder.setThumbnail(this.plugin.getConfig().getString("messages.discord.link_success.thumbnail")
                 .replace("%player-name%", playerName));
-        embedBuilder.setColor(Integer.parseInt(this.plugin.getConfig().getString("messages.discord.link_success.color"), 16));
-        if (this.plugin.getConfig().getBoolean("messages.discord.link_success.timestamp")) {
-            embedBuilder.setTimestamp(new Date().toInstant());
-        }
 
         event.replyEmbeds(embedBuilder.build()).queue();
     }
