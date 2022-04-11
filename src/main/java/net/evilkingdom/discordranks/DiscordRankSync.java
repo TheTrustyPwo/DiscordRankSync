1bb3d174a1643093a850553ce8d767b77b148463
TheTrustyPwo <shucecai@gmail.com>
2022-04-11 11:28:10 +0800

    Secret code generation & handling

--- src/main/java/net/evilkingdom/discordranks/DiscordRankSync.java
+++ src/main/java/net/evilkingdom/discordranks/DiscordRankSync.java
@@ -4,66 +4,109 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+import net.evilkingdom.discordranks.commands.DiscordCommand;
 import net.evilkingdom.discordranks.database.Database;
 import net.evilkingdom.discordranks.database.mongodb.Mongo;
-import net.evilkingdom.discordranks.events.OnPlayerJoin;
-import net.evilkingdom.discordranks.events.OnPlayerQuit;
+import net.evilkingdom.discordranks.events.discord.LinkSlashCommand;
+import net.evilkingdom.discordranks.events.minecraft.OnPlayerJoin;
+import net.evilkingdom.discordranks.events.minecraft.OnPlayerQuit;
 import net.evilkingdom.discordranks.player.PlayerManager;
+import net.evilkingdom.discordranks.utils.StringUtilities;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
+@SuppressWarnings("ConstantConditions")
 public final class DiscordRankSync extends JavaPlugin {
     private static DiscordRankSync instance;
     private Database database;
     private JDA jda;
     private Map<String, Role> ranks;
+    private Map<String, String> messages;
     private PlayerManager playerManager;
+
+    public static DiscordRankSync getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
+        instance = this;
         saveDefaultConfig();
         loadDatabase();
         loadJDA();
+        loadSlashCommands();
         loadRanks();
+        loadMessages();
         this.playerManager = new PlayerManager(this);
+        registerCommands();
         registerEvents();
     }
 
     private void loadDatabase() {
         String databaseType = getConfig().getString("database.type");
+
         switch (databaseType.toUpperCase(Locale.ROOT)) {
-            case "MONGODB": {
-                this.database = new Mongo(this);
-                break;
-            }
+            case "MONGODB" -> this.database = new Mongo(this);
         }
     }
 
     private void loadJDA() {
         JDABuilder builder = JDABuilder.createDefault(getConfig().getString("bot_token"));
+
         builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
         builder.setBulkDeleteSplittingEnabled(false);
         builder.setActivity(Activity.playing("Evil Kingdom"));
+
         try {
             this.jda = builder.build();
-        } catch (LoginException e) {
+            this.jda.awaitReady();
+            getLogger().info("Logged into discord bot");
+        } catch (LoginException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    private void loadSlashCommands() {
+        CommandData linkCommandData = new CommandDataImpl("link", "Link your Minecraft and Discord accounts!")
+                .addOption(OptionType.STRING, "code", "Secret code what was provided to you", true);
+        this.jda.getGuildById(getConfig().getString("guild_id")).upsertCommand(linkCommandData).queue();
+        this.jda.addEventListener(new LinkSlashCommand(this));
+    }
+
     private void loadRanks() {
         this.ranks = new HashMap<>();
+
         ConfigurationSection section = getConfig().getConfigurationSection("ranks");
         for (String permission : section.getKeys(false)) {
-            this.ranks.put(permission, this.jda.getRoleById(section.getString(permission)));
+            this.ranks.put(permission.replace('_', '.'), this.jda.getGuildById(getConfig().getString("guild_id"))
+                    .getRoleById(section.getString(permission)));
         }
+
+        getLogger().info(String.format("Loaded %d ranks", this.ranks.size()));
+    }
+
+    private void loadMessages() {
+        this.messages = new HashMap<>();
+
+        ConfigurationSection section = getConfig().getConfigurationSection("messages.minecraft");
+        for (String identifier : section.getKeys(false)) {
+            String message = StringUtilities.colorize(section.getStringList(identifier));
+            this.messages.put(identifier, message);
+        }
+
+        getLogger().info("Loaded messages");
+    }
+
+    private void registerCommands() {
+        getCommand("discord").setExecutor(new DiscordCommand(this));
     }
 
     private void registerEvents() {
@@ -73,11 +116,8 @@ public final class DiscordRankSync extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-    }
-
-    public static DiscordRankSync getInstance() {
-        return instance;
+        this.database.close();
+        this.jda.shutdown();
     }
 
     public Database getDatabase() {
@@ -90,6 +130,10 @@ public final class DiscordRankSync extends JavaPlugin {
 
     public Map<String, Role> getRanks() {
         return ranks;
+    }
+
+    public String getMessage(String identifier) {
+        return this.messages.get(identifier);
     }
 
     public PlayerManager getPlayerManager() {
--- src/main/java/net/evilkingdom/discordranks/player/PlayerManager.java
+++ src/main/java/net/evilkingdom/discordranks/player/PlayerManager.java
@@ -1,42 +1,77 @@
 package net.evilkingdom.discordranks.player;
 
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
 import net.evilkingdom.discordranks.DiscordRankSync;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class PlayerManager {
     private final DiscordRankSync plugin;
-    private Map<UUID, String> playerUserCache;
-    private BukkitTask updateRoleTask;
+    private final Map<UUID, String> playerUserCache;
+    private final Map<String, UUID> secretCodes;
+    private final long ROLE_UPDATE_PERIOD;
+    private final long CODE_EXPIRY_TIME;
 
     public PlayerManager(DiscordRankSync plugin) {
         this.plugin = plugin;
         this.playerUserCache = new HashMap<>();
+        this.secretCodes = new HashMap<>();
+        this.ROLE_UPDATE_PERIOD = this.plugin.getConfig().getLong("role_update_period") * 60L;
+        this.CODE_EXPIRY_TIME = this.plugin.getConfig().getLong("code_expiry_time") * 60L;
         startUpdateRoleTask();
     }
 
     private void startUpdateRoleTask() {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this.plugin, this::updateRole, 0L, this.ROLE_UPDATE_PERIOD);
+    }
+
+    public void updateRole() {
         String guildId = this.plugin.getConfig().getString("guild_id");
-        this.updateRoleTask = Bukkit.getScheduler().runTaskTimerAsynchronously(this.plugin, () -> {
-            this.playerUserCache.forEach((uuid, userId) -> {
-                Player player = Bukkit.getPlayer(uuid);
-                this.plugin.getRanks().forEach((permission, role) -> {
-                    if (player.hasPermission(permission)) {
-                        this.plugin.getJda().getGuildById(guildId).addRoleToMember(this.playerUserCache.get(player.getUniqueId()), role).queue();
-                    }
-                });
+
+        this.playerUserCache.forEach((uuid, userId) -> {
+            Player player = Bukkit.getPlayer(uuid);
+
+            this.plugin.getRanks().forEach((permission, role) -> {
+                if (player.hasPermission(permission)) {
+                    this.plugin.getJda().getGuildCache().getElementById(guildId)
+                            .addRoleToMember(this.playerUserCache.get(player.getUniqueId()), role).queue();
+                }
             });
-        }, 0L, 0L);
+        });
+    }
+
+    public void link(UUID uuid, String discordId) {
+        this.plugin.getDatabase().linkPlayer(uuid, discordId);
+        this.playerUserCache.put(uuid, discordId);
+    }
+
+    public void unlink(UUID uuid) {
+        this.plugin.getDatabase().unlinkPlayer(uuid);
+        this.playerUserCache.remove(uuid);
+    }
+
+    public String giveCode(UUID uuid) {
+        if (this.secretCodes.containsValue(uuid)) {
+            this.secretCodes.values().remove(uuid);
+        }
+
+        String code = String.valueOf(ThreadLocalRandom.current().nextInt(10000000, 99999999));
+        this.secretCodes.put(code, uuid);
+        Bukkit.getScheduler().runTaskLaterAsynchronously(this.plugin,
+                () -> this.secretCodes.remove(code), this.CODE_EXPIRY_TIME);
+
+        return code;
     }
 
     public Map<UUID, String> getPlayerUserCache() {
         return playerUserCache;
+    }
+
+    public Map<String, UUID> getSecretCodes() {
+        return secretCodes;
     }
 }
