d07add27e24f69b82c77382cdafeafb202fa1ed5
TheTrustyPwo <shucecai@gmail.com>
2023-05-17 18:21:26 +0800

    Update tab completer to show commands based on permission

--- src/main/java/net/evilkingdom/discordranksync/commands/DiscordTabComplete.java
+++ src/main/java/net/evilkingdom/discordranksync/commands/DiscordTabComplete.java
@@ -6,17 +6,21 @@ import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DiscordTabComplete implements TabCompleter {
-    private static final List<String> subCommands = Arrays.asList("help", "link", "unlink", "whois", "reload");
+    private static final List<String> playerCommands = Arrays.asList("help", "link", "unlink", "chat", "whois");
 
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 1) {
-            return subCommands;
+            final List<String> commands = new ArrayList<>(playerCommands);
+            if (sender.hasPermission("drs.reload")) commands.add("reload");
+            if (sender.hasPermission("drs.update")) commands.add("update");
+            return commands;
         }
 
         return null;
